31100302c907e533525df2c0686a52e3ba7c81c3
Alex Zayats <olesmx@gmail.com>
2016-03-28 21:00:58 +0300

    Added hometask for les #8. messenger based on events

--- LITS-UI-Lecture8/main.js
+++ LITS-UI-Lecture8/main.js
@@ -1,5 +1,16 @@
 var listeners = [];
 
+var sendMsgBtn = document.getElementById("send-msg-btn");
+var msg_body = document.getElementById("msg_body");
+var name_sender = document.getElementById("name_sender");
+
+var userList = document.getElementById("user-list");
+var msgBodyBlock = document.getElementById("msgBodyBlock");
+
+var messageTemplate = msgBodyBlock.innerHTML;
+msgBodyBlock.innerHTML = '';
+var MsgDB = [];
+
 function listen(givenEventName, givenCallback){
 	listeners.push({
 		'eventName': givenEventName,
@@ -14,47 +25,61 @@ function trigger(triggeredEventName, eventData){
 	})
 }
 
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var block1 = document.getElementById("block1");
-var block2 = document.getElementById("block2");
-var numMessages = document.getElementById("numMessages");
-var block1 = document.getElementById("senderName");
-var block2 = document.getElementById("messageText");
+function updateMsgBody(user){
+	if (!MsgDB[user])
+		return false;
 
+	msgBodyBlock.innerHTML = "";
+	MsgDB[user].forEach(function(e){
+		msgBodyBlock.innerHTML += messageTemplate.replace('{{msg}}',e.msg).replace('{{time}}',e.time);
+	});
+}
 
-button1.addEventListener("click",function(e){
-	var evenData = {
-		from: 'John',
-		message: 'Hello'
-	};
-
-	trigger("new message",evenData);
-});
-
-button2.addEventListener("click",function(e){
-	var evenData = {
-		from: 'Bill',
-		message: 'Make me a sandwitch'
-	};
-
-	trigger("new message",evenData);
-});
-
-/*block 1 reaction*/
-var numOfMwssages = 0;
-listen('new-message',function block1reaction(eventData){
-	//block1.innerHTML = 'REACTED';
-	block1.style.borderColor = "green";
-	//console.log(eventData);
-	numOfMwssages++;
-	numMessages.innerHTML = numOfMwssages;
-});
-
-/*block 2 reaction*/
-listen('new-message',function block2reaction(msg){
-	//block2.innerHTML = JSON.stringify(eventData);
+sendMsgBtn.addEventListener("click",function(e){
+	if (name_sender.value == "" || msg_body.value == ""){
+		alert('Name and text of message must be filled!');
+		return;
+	}
+	var eventData = {};
+	eventData.from = name_sender.value.trim();
+	eventData.message = msg_body.value;
 	
-	senderName.innerHTML= msg.from;
-	messageText.innerHTML= msg.message;
+	trigger("new message",eventData);
 });
+
+userList.addEventListener("click",function(e){
+	if (e.target.tagName.toLowerCase() == "li"){
+		e.target.classList.remove("msgfrom--item--new");
+		updateMsgBody(e.target.innerHTML);
+	}
+});
+
+
+listen('new-message',function saveToDB(eventData){	
+	var d = new Date();
+
+	if(!MsgDB[eventData.from])
+		MsgDB[eventData.from] = [];
+
+	MsgDB[eventData.from].push({
+		'time': d.toString(),
+		'msg': eventData.message,
+	});
+});
+
+listen('new-message',function updateFromList(msg){
+	userItems = userList.children;
+
+	for (var x in userItems)
+		if (userItems.hasOwnProperty(x))
+	       if (msg.from == userItems[x].innerText){
+	       		userItems[x].classList.add("msgfrom--item--new");
+	       		return;
+	       }
+	el = document.createElement("li");
+	userList.appendChild(el);
+	el.innerHTML = msg.from;
+	el.classList.add("msgfrom--item--new");
+});
+
+//listen('new-message',function updateFromList(msg){
